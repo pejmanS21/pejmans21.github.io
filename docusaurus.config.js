6fb3276dd0bf44e8628875623f082e4f1d58bb9d
pejmans21 <pezhmansamadi21@gmail.com>
2023-04-27 10:39:49 +0330

    Add Algolia Config for search bar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,10 +57,38 @@ const config = {
       }),
     ],
   ],
-
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      algolia: {
+        // The application ID provided by Algolia
+        appId: '80F4KMFHH3',
+  
+        // Public API key: it is safe to commit it
+        apiKey: '3f1d099af44f4410e0efdbebe68b24ce',
+  
+        indexName: 'pejmans21io',
+  
+        // Optional: see doc section below
+        contextualSearch: true,
+  
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: 'external\\.com|domain\\.com',
+  
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        // replaceSearchResultPathname: {
+        //   from: '/docs/', // or as RegExp: /\/docs\//
+        //   to: '/',
+        // },
+  
+        // Optional: Algolia search parameters
+        searchParameters: {},
+  
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+  
+        //... other Algolia params
+      },
       colorMode: {
         disableSwitch: false,
         respectPrefersColorScheme: true,
--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/about">
+            to="/docs">
             About Me
           </Link>
         </div>
